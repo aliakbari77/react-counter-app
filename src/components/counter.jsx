dd699397e0045b6e4664e0b358448c8544b537e7
Ali Akbari <7731.aa@gmail.com>
2022-03-05 12:05:20 +0330

    feat: add getBadgeClasses, formatCount method to handle classes and count

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {  } 
+    state = { 
+        count: 1,
+        tags: ['tag1', 'tag2', 'tag3']
+     } 
     render() { 
+        let tags = this.state.tags.map(tag => <li key={tag}> { tag }</li>)
         return (
             <React.Fragment>
-                <h1>hello react.</h1>
+                <span className={ this.getBadgeClasses() }>{ this.formatCount() }</span>
+                <button className='btn btn-secondary m-2'>Increment</button>
+                <ul>
+                    {tags}
+                </ul>
             </React.Fragment>
         );
+    }
+
+    getBadgeClasses(){
+        let classes = 'badge m-2 badge-'
+        classes += this.state.count === 0 ? 'warning' : 'primary'
+        return classes
+    }
+
+    formatCount(){
+        let { count } = this.state
+        return count === 0 ? 'Zero' : count
     }
 }
  
