e0a07d38c5d347135b1e1e150592a8e32a36077e
Ali Akbari <7731.aa@gmail.com>
2022-03-05 11:53:59 +0330

    feat: add bootstrap, counter.jsx

---
+++ src/components/counter.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <React.Fragment>
+                <h1>hello react.</h1>
+            </React.Fragment>
+        );
+    }
+}
+ 
+export default Counter;
